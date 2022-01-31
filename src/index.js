82dec0d3a59d5bd8998a0c5ebf0ad32514453f07
Nick Levshin <nikita.levshin2016@gamil.com>
2022-01-31 16:27:04 +0300

    feat: add towelSort()

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
+Object.defineProperty(Array.prototype, "flat", {
+    value: function (depth = 1) {
+        return this.reduce(function (flat, toFlatten) {
+            return flat.concat(
+                Array.isArray(toFlatten) && depth > 1
+                    ? toFlatten.flat(depth - 1)
+                    : toFlatten
+            );
+        }, []);
+    },
+});
 
-// You should implement your task here.
+module.exports = function towelSort(matrix) {
+    let result = [];
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+    if (!matrix) {
+        return [];
+    }
+
+    for (let i = 0; i < matrix.length; i++) {
+        if (i % 2 === 0) {
+            result[i] = matrix[i];
+        } else {
+            result[i] = matrix[i].reverse();
+        }
+    }
+
+    console.log(result.flat());
+    return result.flat();
+};
